d4d6a02e8037f80d639ac580af93ad3e0f90fa46
Nahid Hasan <nahid.limu@gmail.com>
2017-11-02 14:30:29 +0600

    Qr scanner added

--- app/src/main/java/com/example/mehedi/qrcode/MainActivity.java
+++ app/src/main/java/com/example/mehedi/qrcode/MainActivity.java
@@ -15,7 +15,13 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.AlertDialog;
+import android.util.Log;
+import android.view.View;
+import com.google.zxing.Result;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
 
     private Button button;
     private EditText editText;
@@ -46,4 +52,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    //QR Scanner
+
+    private ZXingScannerView mScannerView;
+
+
+    public void onClick(View v){
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        //Do anything with result here :D
+        Log.w("handleResult", result.getText());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan result");
+        builder.setMessage(result.getText());
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        //Resume scanning
+        //mScannerView.resumeCameraPreview(this);
+    }
+
+
 }
